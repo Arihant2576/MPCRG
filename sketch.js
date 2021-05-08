d521feaf99af8cfa2f66a41c3ce49cd909830476
Arihant2576 <76998314+Arihant2576@users.noreply.github.com>
2021-05-08 13:02:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var canvas;
+var database;
+var form;
+var game1;
+var player;
+var gamestate = 0;
+var playerCount = 0;
+var AllPlayers;
+
+function setup(){
+    database=firebase.database();
+    canvas = createCanvas(400, 400);
+    game1 = new Game();
+    game1.getState();
+    game1.start();
+}
+
+function draw(){
+    if(playerCount == 4){
+        game1.update(1);
+    }
+    if(gamestate == 1){
+        clear();
+        game1.play();
+    }
+}
